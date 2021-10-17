39687aec795ffedf5270bbb7576457b33ff50776
joaovitor3 <joaovytor0@gmail.com>
2021-10-17 11:04:12 -0300

    Add playlist models

--- playlist_manager/models.py
+++ playlist_manager/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Playlist(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Track(models.Model):
+    name = models.CharField(max_length=200)
+    popularity = models.PositiveSmallIntegerField()
+    duration = models.PositiveIntegerField()
+    artists = models.CharField(max_length=200)
+    album_name = models.CharField(max_length=200)
+    album_image = models.CharField(max_length=200)
+    playlists = models.ManyToManyField(Playlist)
+
+    def __str__(self):
+        return self.name
